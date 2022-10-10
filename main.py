2a6bcb8ef39295900979803c25ca2c1dc7d35839
William Redenbaugh <wmredenbaugh@gmail.com>
2022-10-09 22:10:18 -0700

    don't want to just publicly view credentials

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ PASSWORD = ""
 
 def get_credentials_json():
     global USERNAME, PASSWORD
-    
+
     # get current directory for file
     file_dir = os.path.dirname(os.path.realpath('__file__'))
     filename = os.path.join(file_dir, "credentials.json")
@@ -20,7 +20,7 @@ def get_credentials_json():
     # Open and dump file
     file = open(filename)
     file_contents = file.read()
-    print(file_contents)
+    print("Credentials Entered")
     file.close()
 
     # Decode JSON and save credentials in memory
@@ -29,11 +29,11 @@ def get_credentials_json():
     PASSWORD = str(decoded_json["password"])
 
 def main():
-    global USERNAME, PASSWORD 
+    global USERNAME, PASSWORD
 
     # Get latest credentials from the json "credentials.json"
     get_credentials_json()
-    
+
     # Initialize Waveshare Python Library
     display = EPD()
     display.init()
@@ -41,10 +41,10 @@ def main():
     font2 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 30)
     image = Image.new('1', (display.height, display.width), 255)
     draw = ImageDraw.Draw(image)
-    
+
     # Initialize Dexcom Glucose Measurement REST API
     dexcom = Dexcom(USERNAME, PASSWORD)
-    
+
     glucose_text_draw = ImageDraw.Draw(image)
     glucose_text_draw.text((15, 120), "Current Glucose", font=font2, fill=0)
     while True:
